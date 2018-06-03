b8333229c305e446c8b77196afb87021f99be92f
Sathish Kumar Parthasarathy <p.sathish.cs@gmail.com>
2018-06-02 21:42:34 -0500

    fix streaming

--- src/main/java/org/hello/bigdata/spark/HelloSparkStreaming.java
+++ src/main/java/org/hello/bigdata/spark/HelloSparkStreaming.java
@@ -3,6 +3,7 @@ package org.hello.bigdata.spark;
 import java.util.Arrays;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
@@ -10,14 +11,16 @@ import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
 public class HelloSparkStreaming {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		SparkConf conf = new SparkConf().setMaster("spark://quickstart.cloudera:7077").setAppName("HelloSpark");
 		JavaStreamingContext streamContext = new JavaStreamingContext(conf, Durations.seconds(1));
-		JavaReceiverInputDStream<String> lines = streamContext.("localhost", 9999);
-		JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(x.split(" ")).iterator());
-		JavaPairDStream<String, Integer> pairs = words.mapToPair(s -> new Tuple2<>(s, 1));
-		JavaPairDStream<String, Integer> wordCounts = pairs.reduceByKey((i1, i2) -> i1 + i2);
-		wordCounts.print();
+		JavaReceiverInputDStream<String> lines = streamContext.socketTextStream("localhost", 9999);
+		JavaDStream<String> words = lines.filter(
+				new Function<String, Boolean>(){
+					public Boolean call(String x) { return x.contains("Hello");}
+				}	
+		);
+		words.print();
 		streamContext.start();
 		streamContext.awaitTermination();
 		
